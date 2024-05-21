fc5b896b0c0e202a6e3487c88fed324a75e07b41
GMartin03 <martingomez2003174@hotmail.com>
2024-05-21 14:41:13 -0300

    created structure of the body

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
-import { Col, Row } from 'react-bootstrap'
-import '../styles/Footer.css'
+import { Col, Row } from "react-bootstrap";
+import "../styles/Footer.css";
 
 export const Footer = () => {
   return (
-    <div className='footer-container'>
-      <Row className='text-center'>
-        <Col sm={12} lg={6}>Eoliasur, fundada en 2011, nace con la visión de ser un actor clave en la transformación de la matriz energética a través del desarrollo de energías renovables.</Col>
+    <div className="footer-container">
+      <Row className="text-center">
+        <Col sm={12} lg={6}>
+          Eoliasur, fundada en 2011, nace con la visión de ser un actor clave en
+          la transformación de la matriz energética a través del desarrollo de
+          energías renovables.
+        </Col>
         <Col sm={12} lg={6}>
           <p>Sinclair 3244, 2do piso, CABA, Argentina</p>
           <p>(+5411) 4774 8567</p>
@@ -13,8 +17,11 @@ export const Footer = () => {
         </Col>
       </Row>
       <Row>
-        <Col>Copyright Eolia Renovables S.A. 2024 - All Rights Reserved | Desarrollado por JTDIGITAL</Col>
+        <Col>
+          Copyright Eolia Renovables S.A. 2024 - All Rights Reserved |
+          Desarrollado por JTDIGITAL
+        </Col>
       </Row>
     </div>
-  )
-}
+  );
+};
