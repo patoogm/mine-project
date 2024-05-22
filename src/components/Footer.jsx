76a0dbc8c87cd39409f55c33fcce95ac48541f84
GMartin03 <martingomez2003174@hotmail.com>
2024-05-22 13:29:39 -0300

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,38 @@ import "../styles/Footer.css";
 export const Footer = () => {
   return (
     <div className="footer-container">
-      <Row className="text-center">
-        <Col sm={12} lg={6}>
-          Eoliasur, fundada en 2011, nace con la visión de ser un actor clave en
-          la transformación de la matriz energética a través del desarrollo de
-          energías renovables.
+      <Row sm={12}>
+        <Col sm={12}>
+          <h1>Eoliasur</h1>
         </Col>
         <Col sm={12} lg={6}>
-          <p>Sinclair 3244, 2do piso, CABA, Argentina</p>
-          <p>(+5411) 4774 8567</p>
-          <p>info@eoliasur.com</p>
+          <p>
+            Eoliasur, fundada en 2011, nace con la visión de ser un actor clave
+            en la transformación de la matriz energética a través del desarrollo
+            de energías renovables.
+          </p>
+        </Col>
+        <Col sm={12} lg={6}>
+          <Col>
+            <h2>Contactanos</h2>
+          </Col>
+          <Col>
+            <p>Sinclair 3244, 2do piso, CABA, Argentina</p>
+          </Col>
+          <Col>
+            <p>(+5411) 4774 8567</p>
+          </Col>
+          <Col>
+            <p>info@eoliasur.com</p>
+          </Col>
         </Col>
       </Row>
-      <Row>
+      <Row sm={12}>
         <Col>
-          Copyright Eolia Renovables S.A. 2024 - All Rights Reserved |
-          Desarrollado por JTDIGITAL
+          <p>
+            Copyright Eolia Renovables S.A. 2024 - All Rights Reserved |
+            Desarrollado por JTDIGITALT
+          </p>
         </Col>
       </Row>
     </div>
