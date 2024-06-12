7daa9491ee95ccf4478dee412254bd249c506f7f
GMartin03 <martingomez2003174@hotmail.com>
2024-06-12 11:36:18 -0300

    arreglada imagen de contacto y diseño del panel de admin

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -3,58 +3,47 @@ import Form from "react-bootstrap/Form";
 import "../styles/AdminPanel.css";
 
 export const AdminPanel = () => {
-
   return (
     <div>
-      <section className="w-100">
-        <Form className="w-50">
-          <Form.Group className="mb-3 w-25" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+      <section className="w-100 d-flex flex-column justify-content-center align-items-center bg-secondary  pb-4">
+        <h1>Panel de Administración</h1>
+        <Form className="w-50 bg-light d-flex flex-column align-items-center">
+          <Form.Group className="p-3 w-100" controlId="formBasicEmail">
+            <Form.Label>Nombre del proyecto</Form.Label>
+            <Form.Control type="text" placeholder="Normal text" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group className="p-3 w-100" controlId="formBasicPassword">
+            <Form.Label>Ubicación</Form.Label>
+            <Form.Control type="text" placeholder="Normal text" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="p-3 w-100" controlId="formBasicPassword">
             <Form.Label>Parrafo</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="text" placeholder="Normal text" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="p-3 w-100" controlId="formBasicPassword">
             <Form.Label>Parrafo</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="text" placeholder="Normal text" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="p-3 w-100" controlId="formBasicPassword">
             <Form.Label>Parrafo</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="text" placeholder="Normal text" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="p-3 w-100" controlId="formBasicPassword">
             <Form.Label>Parrafo</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="text" placeholder="Normal text" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="p-3 w-100" controlId="formBasicPassword">
             <Form.Label>Parrafo</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="text" placeholder="Normal text" />
           </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Parrafo</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Parrafo</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-
-          <Button variant="primary" type="button">
-            Submit
+          <Button variant="info" type="button" className="pb-2">
+            Enviar
           </Button>
         </Form>
       </section>
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,12 +2,13 @@ import { ContactForm } from "../components/ContactForm";
 import { Footer } from "../components/Footer";
 import { NavbarComponent } from "../components/Navbar";
 import "../styles/Main.css";
+import Imagen5 from "../statics/imagen5.jpg";
 
 export const ContactUs = () => {
   return (
     <>
       <NavbarComponent />
-      <div className="imgContactPage"></div>
+      <img src={Imagen5} className="imgContactPage"></img>
       <ContactForm />
       <Footer />
     </>
